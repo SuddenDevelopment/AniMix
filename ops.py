beb7d9916bd6bb4ca6ae758d6754a8825bbc21d5
SuddenDevelopment <anthony.aragues@gmail.com>
2023-01-02 16:07:27 -0500

    add mesh for blank keyframe

--- ops.py
+++ ops.py
@@ -67,12 +67,24 @@ class KEY_OT_BlankKey(bpy.types.Operator):
         for obj in context.selected_objects:
             keyframes.nudgeFrames(
                 obj, context.scene.frame_current, 1, False)
+            # could definitely refactor some stuff here to not repeat
             if obj.type == 'CURVE':
                 intSwapId = obj.get('key_id')
                 intSwapObjectID = actions.getNextSwapObjectId(obj)
                 strNewObjName = actions.getSwapObjectName(
                     intSwapId, intSwapObjectID)
                 objData = bpy.data.curves.new(name=strNewObjName, type='CURVE')
+                objTmp = bpy.data.objects.new(strNewObjName, objData)
+                objTmp.data.use_fake_user = True
+                objTmp["key_id"] = intSwapId
+                actions.setSwapKey(obj, intSwapObjectID,
+                                   context.scene.frame_current+1, update=False)
+            elif obj.type == 'MESH':
+                intSwapId = obj.get('key_id')
+                intSwapObjectID = actions.getNextSwapObjectId(obj)
+                strNewObjName = actions.getSwapObjectName(
+                    intSwapId, intSwapObjectID)
+                objData = bpy.data.meshes.new(name=strNewObjName, type='MESH')
                 objTmp = bpy.data.objects.new(strNewObjName, objData)
                 objTmp.data.use_fake_user = True
                 objTmp["key_id"] = intSwapId
