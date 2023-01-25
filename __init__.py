f65bdfefde78e7040f3bee40396d09f0f5ac87e6
SuddenDevelopment <anthony.aragues@gmail.com>
2023-01-25 01:36:43 -0500

    update n panel big time

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import bpy
 import time
 import sys
+import os
 from . import ops
 from . import actions
 from . import prefs
@@ -37,6 +38,38 @@ class PanelProps(bpy.types.PropertyGroup):
     btnRemoTime: bpy.props.IntProperty(default=0)
 
 
+# ICONS
+icons_collection = None
+this_directory = os.path.dirname(__file__)
+
+
+def load_icons():
+    global icons_collection
+    icons_collection = bpy.utils.previews.new()
+    arrIcons = [
+        'pin_frame',
+        'clear_key',
+        'insert_key',
+        'remove_key',
+        'blank_key',
+        'clone_key',
+        'clone_object',
+        'clone_object_blank_keys',
+        'add_space',
+        'remove_space',
+        'no_space',
+        'set_space',
+        'separate_objects',
+        'combine_objects',
+        'show_panel',
+        'add_asset'
+    ]
+    for strIcon in arrIcons:
+        strFileName = f'{this_directory}/icons_16/{strIcon}.png'
+        icons_collection.load(strIcon, strFileName, "IMAGE")
+        print(this_directory, strFileName, icons_collection[strIcon].icon_id)
+
+
 class KEY_PT_Main(bpy.types.Panel):
     bl_label = "StopMotion"
     bl_category = "Animate"
@@ -46,35 +79,55 @@ class KEY_PT_Main(bpy.types.Panel):
     # create the panel
 
     def draw(self, context):
+        global icons_collection
         layout = self.layout
+        row = layout.row()
+        row.label(text="Onion Skin")
+        row.operator("key.pin_frames", text="PIN",
+                     icon_value=icons_collection["pin_frame"].icon_id)
         row = layout.row()
         row.label(text="StopMotion Keys")
         row = layout.column_flow(columns=3)
-        row.operator("key.insert_key", text="ADD")
-        row.operator("key.remove_key", text="DEL")
-        row.operator("key.blank_key", text="BLA")
+        row.operator("key.insert_key", text="ADD",
+                     icon_value=icons_collection["insert_key"].icon_id)
+        row.operator("key.remove_key", text="DEL",
+                     icon_value=icons_collection["remove_key"].icon_id)
+        row.operator("key.blank_key", text="BLA",
+                     icon_value=icons_collection["blank_key"].icon_id)
         row = layout.row()
         row.label(text="Duplicate")
         row = layout.column_flow(columns=3)
-        row.operator("key.clone_key", text="KEY")
-        row.operator("key.clone_unique_key", text="UNI")
-        row.operator("key.clone_object", text="OBJ")
+        row.operator("key.clone_key", text="KEY",
+                     icon_value=icons_collection["clone_key"].icon_id)
+        row.operator("key.clone_object", text="OBJ",
+                     icon_value=icons_collection["clone_object"].icon_id)
+        row.operator("key.clone_object_blank_keys", text="BLA",
+                     icon_value=icons_collection["clone_object_blank_keys"].icon_id)
         row = layout.row()
         row.label(text="Frame Spacing")
         row = layout.column_flow(columns=3)
-        row.operator("key.add_space", text="ADD")
-        row.operator("key.remove_space", text="DEL")
-        row.operator("key.set_space", text="SET")
-        row = layout.row()
+        row.operator("key.add_space", text="ADD",
+                     icon_value=icons_collection["add_space"].icon_id)
+        row.operator("key.remove_space", text="DEL",
+                     icon_value=icons_collection["remove_space"].icon_id)
+        row.operator("key.no_space", text="NO",
+                     icon_value=icons_collection["no_space"].icon_id)
+        row = layout.row(align=True)
         row.prop(context.scene, "KEY_frameSpace")
+        row.operator("key.set_space", text="SET",
+                     icon_value=icons_collection["set_space"].icon_id)
+
         row = layout.row()
         row.label(text="Frame Objects")
         row = layout.column_flow(columns=3)
-        row.operator("key.separate_objects", text="SEP")
-        row.operator("key.combine_objects", text="COM")
-        row.operator("key.merge_data", text="MER")
+        row.operator("key.separate_objects", text="SEP",
+                     icon_value=icons_collection["separate_objects"].icon_id)
+        row.operator("key.combine_objects", text="COM",
+                     icon_value=icons_collection["combine_objects"].icon_id)
         row = layout.row()
-        row.operator("key.add_asset")
+        row.label(text="Asset Library")
+        row.operator("key.add_asset", text="SET",
+                     icon_value=icons_collection["add_asset"].icon_id)
         row = layout.row()
         row.separator()
         if context.space_data.type == 'VIEW_3D':
@@ -83,11 +136,11 @@ class KEY_PT_Main(bpy.types.Panel):
             # -- remote control switch button
             if remoteVisible:
                 op = self.layout.operator(
-                    'key.viewport_panel', text="Hide Viewport Panel")
+                    'key.viewport_panel', text="Hide Viewport Panel", icon_value=icons_collection["show_panel"].icon_id)
             else:
                 # Make sure the button starts turned off every time
                 op = self.layout.operator(
-                    'key.viewport_panel', text="Show Viewport Panel")
+                    'key.viewport_panel', text="Show Viewport Panel", icon_value=icons_collection["show_panel"].icon_id)
         if bpy.context.window_manager.KEY_message != "":
             box = layout.box()
             row = box.row(align=True)
@@ -133,6 +186,7 @@ addon_keymaps = []
 def register():
     for i in arrClasses:
         bpy.utils.register_class(i)
+    load_icons()
     bpy.app.handlers.load_post.append(onFrame_handler)
     bpy.app.handlers.frame_change_post.clear()
     bpy.app.handlers.frame_change_post.append(actions.onFrame)
@@ -193,3 +247,4 @@ def unregister():
     del bpy.types.Scene.KEY_frameSpace
     del bpy.types.WindowManager.KEY_UI
     del bpy.types.WindowManager.KEY_message
+    bpy.utils.previews.remove(icons_collection)
