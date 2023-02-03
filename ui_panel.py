7da4060e757753bcb467a7c1d22392c1cc3602f0
Adam Earle <MrAdamEarle@gmail.com>
2023-02-03 23:36:40 +1100

    Update ui_panel.py

--- ui_panel.py
+++ ui_panel.py
@@ -85,7 +85,7 @@ class KEY_OT_draw_operator(BL_UI_OT_draw_operator):  # in: bl_ui_draw_op.py ##
                 "name": 'StopMotion Keys',
                 "buttons": {"clear_key":
                             {
-                                "description": (BOLD + "Clear keyframes:\n") +
+                                "description": "Clear keyframes:\n"+
                                 "\n"
                                 "Clears the id data from the current StopMotion object. Keyframe is not deleted.\n" +
                                 "\n"
